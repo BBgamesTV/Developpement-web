0d776693b527ceaf1e4785d375c7e3259bd62fae
BBgamesTV <57633464+BBgamesTV@users.noreply.github.com>
2023-11-24 10:57:04 +0100

    Mission 7

--- TP5/index.php
+++ TP5/index.php
@@ -7,13 +7,16 @@ $titre = 'Noms et prénoms des étudiants';
 
 // les données avec noms de famille
 $etudiants = [
-    ['prénom' => 'Alice', 'nom' => 'Doe', 'passions' => ['tennis','vélo','tuto makeup']],
-    ['prénom' => 'Bob', 'nom' => 'Smith', 'passions' => ['natation','cinéma','infromatique']],
-    ['prénom' => 'Eve', 'nom' => 'Johnson', 'passions' => ['live npc','php','le flambeau']],
-    ['prénom' => 'Malaury'],
-];
+    ['prénom' => 'Alice', 'nom' => 'Doe', 'passions' => ['tennis','vélo','tuto makeup'], 'notes' => ["modules" => [ "Algo"=>10, "Math"=>15, "Anglais"=>8 ]]],
+    ['prénom' => 'Bob', 'nom' => 'Smith', 'passions' => ['natation','cinéma','infromatique'], 'notes' => ["modules" => ["Algo"=> 17,"Math"=>15,"Anglais"=>18]]],
+    ['prénom' => 'Eve', 'nom' => 'Johnson', 'passions' => ['live npc','php','le flambeau'], 'notes' => ["modules" => [ "Algo"=> 13, "Math"=>15 , "Anglais"=>4 ]]],
+    ['prénom' => 'Malaury']];
 
 // 2. Transmettez $titre à la fonction render
 // 3. Utilisez une balise Twig pour afficher le titre dans le template
 echo $twig->render('index.twig.html', ['etudiants' => $etudiants, 'titre' => $titre]);
 ?>
+
+
+<!--  -->
+<!-- , -->
