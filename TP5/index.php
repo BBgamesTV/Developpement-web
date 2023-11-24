7c1c402e1c982cd75c3143a2622bfc8d2d804e36
BBgamesTV <57633464+BBgamesTV@users.noreply.github.com>
2023-11-24 09:18:01 +0100

    TP5

--- TP5/index.php
+++ TP5/index.php
@@ -1,15 +1,19 @@
 <?php
 // on inclut le code de bootstrap.php
-require_once __DIR__.'./bootstrap.php';
-// les données
+require_once __DIR__.'/bootstrap.php';
+
+// 1. Créez une variable $titre
+$titre = 'Noms et prénoms des étudiants';
+
+// les données avec noms de famille
 $etudiants = [
-[ 'prénom' => 'Alice' ],
-[ 'prénom' => 'Bob' ],
-[ 'prénom' => 'Eve' ],
+    ['prénom' => 'Alice', 'nom' => 'Doe', 'passion' => ['tennis','vélo','tuto makeup']],
+    ['prénom' => 'Bob', 'nom' => 'Smith', 'passion' => ['natation','cinéma','infromatique']],
+    ['prénom' => 'Eve', 'nom' => 'Johnson', 'passion' => ['live npc','','tuto makeup']],
+    ['prénom' => 'Malaury'],
 ];
-// On fabrique le rendu de la vue
-// en utilisant le gabarit (template en anglais) index.twig.html
-// en transmettant les données contenues dans le tableau associatif $etudiants
-// Le rendu de la vue est envoyé au navigateur en faisant un "echo"
-echo $twig->render('index.twig.html', ['etudiants' => $etudiants] );
+
+// 2. Transmettez $titre à la fonction render
+// 3. Utilisez une balise Twig pour afficher le titre dans le template
+echo $twig->render('index.twig.html', ['etudiants' => $etudiants, 'titre' => $titre]);
 ?>
